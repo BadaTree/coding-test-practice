fcaa64e04c4e3531f524b9a16a9ed31de755b049
BadaTree <zzzxxx3166@naver.com>
2024-08-25 11:46:44 +0900

    Update Back tracking.py file

--- Algorithm_DataStuctures/Backtracking.py
+++ Algorithm_DataStuctures/Backtracking.py
@@ -1,1 +1,15 @@
 # [ ] Backtracking에 대해 이해해보자 
+
+# [1] Backtracking
+
+# [2] Backtracking 부분 합
+
+# [3] Backtracking N-Queen
+
+# [ ] Backtracking 저자 추천 문제
+# Programmers, 피로도 level 2
+# https://school.programmers.co.kr/learn/courses/30/lessons/87946
+# Programmers, N-Queen, level2
+# https://school.programmers.co.kr/learn/courses/30/lessons/12952
+# Programmers, 양궁대회, level2
+# https://school.programmers.co.kr/learn/courses/30/lessons/92342
