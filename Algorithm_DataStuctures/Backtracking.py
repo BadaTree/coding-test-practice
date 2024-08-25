c187f55bfb8ec4bf72b2833f7238c98b53a99823
BadaTree <zzzxxx3166@naver.com>
2024-08-25 20:20:24 +0900

    Solve N-Queen problem on Programmers (Level 2)

--- Algorithm_DataStuctures/Backtracking.py
+++ Algorithm_DataStuctures/Backtracking.py
@@ -9,7 +9,32 @@
 # [ ] Backtracking 저자 추천 문제
 # Programmers, 피로도 level 2
 # https://school.programmers.co.kr/learn/courses/30/lessons/87946
+
+
 # Programmers, N-Queen, level2
 # https://school.programmers.co.kr/learn/courses/30/lessons/12952
+def solution(n):
+    def is_safe(queens, row, col):
+        for r in range(row):
+            if queens[r] == col or \
+               queens[r] - r == col - row or \
+               queens[r] + r == col + row:
+                return False
+        return True
+    
+    def solve(queens, row):
+        if row == n:
+            return 1
+        count = 0
+        for col in range(n):
+            if is_safe(queens, row, col):
+                queens[row] = col
+                count += solve(queens, row + 1)
+        return count
+    
+    queens = [-1] * n
+    return solve(queens, 0)
+
+
 # Programmers, 양궁대회, level2
 # https://school.programmers.co.kr/learn/courses/30/lessons/92342
