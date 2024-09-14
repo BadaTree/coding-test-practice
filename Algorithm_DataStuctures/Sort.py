0e0e10045a1091e9c13887eb3df340c74ebd6968
BadaTree <zzzxxx3166@naver.com>
2024-09-14 22:58:25 +0900

    Update Counting Sort Algorithm for Improved Efficiency and Clarity

--- Algorithm_DataStuctures/Sort.py
+++ Algorithm_DataStuctures/Sort.py
@@ -85,6 +85,7 @@ print(array)
 '''
 
 # 파이썬의 장점을 살린 방법
+'''
 array = [5,7,9,0,3,1,6,2,4,8]
 
 def quick_sort(array):
@@ -102,3 +103,23 @@ def quick_sort(array):
     return quick_sort(left_array)+ [pivot] + quick_sort(right_array)
 
 print(quick_sort(array))
+'''
+
+# [5] 계수 정렬 
+# 특정한 조건이 부합할 때만 사용할 수 있지만 매우 빠르게 동작하는 정렬 알고리즘.
+# 계수 정렬은 데이터의 크기 범위가 제한되어 정수 형태로 표현할 수 있을 때 사용 가능하다.
+# 데이터의 개수가 N, 데이터(양수) 중 최댓값이 K일 때 최악의 경우에도 수행 시간 O(N+K)를 보장한다.
+# 인덱스 : 데이터의 값, 값: 데이터의 빈도 수 
+
+# 모든 원소의 값이 0보다 크거나 같다고 가정
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2 ]
+# 모든 범위를 포함하는 리스트 선언 (모든 값은 0으로 초기화)
+count_list = [0]*(max(array)+1)
+
+# 데이터의 갯수 카운트 데이터 값에 해당하는 인덱스의 값 +1
+for i in array:
+    count_list[i] += 1 # 각 데이터에 해당하는 인덱스의 값 증가 
+    
+for i in range(len(count_list)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count_list[i]):
+        print(i, end=' ' ) # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력 
