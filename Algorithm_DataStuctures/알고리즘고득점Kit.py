4145e7e33649b8e3d5cd9a67846646f382d3b4c1
BadaTree <zzzxxx3166@naver.com>
2024-10-04 19:01:30 +0900

    commit

--- Algorithm_DataStuctures/알고리즘고득점Kit.py
+++ Algorithm_DataStuctures/알고리즘고득점Kit.py
@@ -1,4 +1,26 @@
 # 1️⃣ 깊이 /너비 우선 탐색
+# [1] 타겟넘버
+# 종료 시점 : numbers가 끝날 때 
+# return +1 : 종료 시점에서 타겟의 값과 같을 때.
+
+def DFS (numbers, target,idx, sum_idx):
+    if idx == len(numbers): # 모든 수 연산 끝날 때
+        if sum_idx == target:
+            return 1
+        else:
+            return 0
+    else:
+        count = 0 
+        count += DFS (numbers, target,idx+1, sum_idx+numbers[idx])
+        count += DFS (numbers, target,idx+1, sum_idx-numbers[idx])
+        return count
+
+def solution(numbers, target):
+    sol_count = 0
+    sol_count = DFS (numbers, target,0, 0)
+    print(sol_count)
+    return DFS (numbers, target,0, 0)
+solution([4, 1, 2, 1],2)
 
 # 2️⃣ 해시
 
