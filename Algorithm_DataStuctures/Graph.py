6999b2459092e45bd571420a9995a47aeb802841
BadaTree <zzzxxx3166@naver.com>
2024-10-05 21:40:39 +0900

    commit

--- Algorithm_DataStuctures/Graph.py
+++ Algorithm_DataStuctures/Graph.py
@@ -1,132 +1,6 @@
 # [ ] Graph에 대해 이해해보자 
 
-# 스택 구현하기 
-stack = []
-stack.append(5)
-stack.append(2)
-stack.append(3)
-stack.append(7)
-stack.pop()
-stack.append(1)
-stack.append(4)
-stack.pop()
-'''
-print("Stack 구현예제")
-print(stack) # 선입부터 # 최하단 원소부터 
-print(stack[::-1]) # 후입부터 # 최상단 원소부터
-'''
-# 깊이 우선 탐색 스택으로 구현 (선입후출) # 1 2 7 6 8 3 4 5
-            
-def DFS(graph,v,visited):
-    visited[v] = True
-    print(v,end=' ')
-    
-    for i in graph[v]:
-        if not visited[i]:
-            visited[i] = True
-            DFS(graph,i,visited)
 
-# 최단 경로             
-# 큐 구현하기 
-from collections import deque
-
-def BFS(graph,v,visited):
-    queue = deque([v])
-    visited_bfs[v] = True
-    
-    while queue:
-        v = queue.popleft()
-        print(v,end=' ')
-        for i in graph[v]:
-            if not visited_bfs[i]:
-                queue.append(i)
-                visited_bfs[i] = True
-
-
-graph = [[],
-         [2,3,8],
-         [1,7],
-         [1,4,5],
-         [3,5],
-         [3,4],
-         [7],
-         [2,6,8],
-         [1,7]
-    ]
-
-visited = [False] * 9
-visited_bfs = [False] * 9 
-
-# DFS(graph,1,visited)
-# print()
-# BFS(graph,1,visited_bfs)
-queue = deque([])
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
-'''
-print("queue 구현 예제")
-print(queue) # 선입부터
-queue.reverse() # 순서 반대로 바꾸기
-print(queue) # 후입부터
-'''
-
-# 재귀함수 구현하기
-# 마치 스택에 쌓듯이 실행 함수들을 차례대로 추가하고, 가장 최근에 실행된부터 종료 
-def recursive_function(i):
-    # 100번째 호출 했을 때 종료되도록 종료 조건 명시
-    if i == 100:
-        return 
-    print(i,'번째 재귀함수를 호출합니다')
-    recursive_function(i+1)
-    print(i,'번째 재귀함수를 종료합니다')
-
-# recursive_function(1)
-
-# 재귀함수 예시1 ) 팩토리얼 구현하기
-def factorial_iterative(n):
-    result = 1
-    for i in range(1, n+1):
-        result *= i
-    return result
-def factorial_recursive(n):
-    # 종료 조건 : n이 1이 되었을 때
-    if n < 1:
-        return 1
-    # 반복 조건 : n을 -1하며 차례대로 곱한다
-    return n*factorial_recursive(n-1) 
-'''
-print(f"iterative {factorial_iterative(5)}")
-print(f"recursive {factorial_recursive(5)}")
-'''
-# 재귀함수 예시2 ) 최대 공약수 계산 (유클리드 호제법)
-# 192와 162의 공약수구하기
-
-# 기본적인 방법으로 풀이 시간: O(min(a,b)), 공간 : O(1)
-def GCD (a,b):
-    gcd = 1
-    n = min(a,b)
-    for i in range(1,n+1):
-        if a% i == 0 and b % i == 0 :
-            gcd = i
-                
-    
-    return gcd
-# print("basic", GCD(192,162))
-
-# 유클리드 호제법 :
-def Euclidean(a,b):
-    r = a%b
-    if r == 0 :
-        return b
-    return Euclidean(b, r)
-
-# print(f"Euclidean {Euclidean(192,162)}")
 # NOTE : 재귀 함수는 반복문을 이용하여 동일한 기능을 구현 가능
 # 스택 대신 재귀함수를 이용(재귀함수를 연속적으로 호출하면 컴퓨터 메모리에 내부에 스택 프레임에 쌓여). 
 # 재귀함수도 스택처럼 LIFO 나중에 실행된 함수부터 종료된다.
@@ -448,3 +322,131 @@ print(bfs(0,0))
 
 # [2] 가장 긴 바이토닉 부분 수열(골드5)
 # https://www.acmicpc.net/problem/11054
+
+# 스택 구현하기 
+stack = []
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+'''
+print("Stack 구현예제")
+print(stack) # 선입부터 # 최하단 원소부터 
+print(stack[::-1]) # 후입부터 # 최상단 원소부터
+'''
+# 깊이 우선 탐색 스택으로 구현 (선입후출) # 1 2 7 6 8 3 4 5
+            
+def DFS(graph,v,visited):
+    visited[v] = True
+    print(v,end=' ')
+    
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = True
+            DFS(graph,i,visited)
+
+# 최단 경로             
+# 큐 구현하기 
+from collections import deque
+
+def BFS(graph,v,visited):
+    queue = deque([v])
+    visited_bfs[v] = True
+    
+    while queue:
+        v = queue.popleft()
+        print(v,end=' ')
+        for i in graph[v]:
+            if not visited_bfs[i]:
+                queue.append(i)
+                visited_bfs[i] = True
+
+
+graph = [[],
+         [2,3,8],
+         [1,7],
+         [1,4,5],
+         [3,5],
+         [3,4],
+         [7],
+         [2,6,8],
+         [1,7]
+    ]
+
+visited = [False] * 9
+visited_bfs = [False] * 9 
+
+# DFS(graph,1,visited)
+# print()
+# BFS(graph,1,visited_bfs)
+queue = deque([])
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+'''
+print("queue 구현 예제")
+print(queue) # 선입부터
+queue.reverse() # 순서 반대로 바꾸기
+print(queue) # 후입부터
+'''
+
+# 재귀함수 구현하기
+# 마치 스택에 쌓듯이 실행 함수들을 차례대로 추가하고, 가장 최근에 실행된부터 종료 
+def recursive_function(i):
+    # 100번째 호출 했을 때 종료되도록 종료 조건 명시
+    if i == 100:
+        return 
+    print(i,'번째 재귀함수를 호출합니다')
+    recursive_function(i+1)
+    print(i,'번째 재귀함수를 종료합니다')
+
+# recursive_function(1)
+
+# 재귀함수 예시1 ) 팩토리얼 구현하기
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+def factorial_recursive(n):
+    # 종료 조건 : n이 1이 되었을 때
+    if n < 1:
+        return 1
+    # 반복 조건 : n을 -1하며 차례대로 곱한다
+    return n*factorial_recursive(n-1) 
+'''
+print(f"iterative {factorial_iterative(5)}")
+print(f"recursive {factorial_recursive(5)}")
+'''
+# 재귀함수 예시2 ) 최대 공약수 계산 (유클리드 호제법)
+# 192와 162의 공약수구하기
+
+# 기본적인 방법으로 풀이 시간: O(min(a,b)), 공간 : O(1)
+def GCD (a,b):
+    gcd = 1
+    n = min(a,b)
+    for i in range(1,n+1):
+        if a% i == 0 and b % i == 0 :
+            gcd = i
+                
+    
+    return gcd
+# print("basic", GCD(192,162))
+
+# 유클리드 호제법 :
+def Euclidean(a,b):
+    r = a%b
+    if r == 0 :
+        return b
+    return Euclidean(b, r)
+
+# print(f"Euclidean {Euclidean(192,162)}")
