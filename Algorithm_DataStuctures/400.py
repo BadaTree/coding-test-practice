026fa98df8c00dfbc292279c677e3f097e152b9e
BadaTree <zzzxxx3166@naver.com>
2024-10-16 23:11:18 +0900

    Update solution for the 1, 2, 3 Sum problem to improve clarity and efficiency.

--- Algorithm_DataStuctures/400.py
+++ Algorithm_DataStuctures/400.py
@@ -26,18 +26,44 @@ print(dp[N])
 # 2×n 타일링 2
 
 # 1, 2, 3 더하기
-import itertools
+# 내 풀이 :  창의적이지만, 성능이 많이 떨어집니다.
+# import itertools
 
-data = [1, 2, 3]
-length = 2
-end = 
-# Generate permutations allowing repetition
-for i in range(1,5):
-    permutations = itertools.product(data, repeat=i)
+# T = int(input())
+# data = [1, 2, 3]
 
-    for perm in permutations:
-        if sum(perm) == 4: 
-            print(perm) 
+# for _ in range(T):
+#     n = int(input())
+#     count = 0
+#     # Generate permutations allowing repetition
+#     for i in range(1,n+1):
+#         permutations = itertools.product(data, repeat=i)
+
+#         for perm in permutations:
+#             if sum(perm) == n: 
+#                 count += 1
+#     print(count)
+
+
+# GPT 
+T = int(input())
+cases = [int(input()) for _ in range(T)]
+
+# dp 배열 준비
+dp = [0] * 12  # n이 11보다 작으므로 dp[0]부터 dp[10]까지 필요
+dp[0] = 1  # 기본 사례: 0을 만드는 방법 1개
+
+# dp 배열 채우기
+for i in range(1, 11):
+    dp[i] = dp[i - 1]
+    if i >= 2:
+        dp[i] += dp[i - 2]
+    if i >= 3:
+        dp[i] += dp[i - 3]
+
+# 각 테스트 케이스에 대한 결과 출력
+for n in cases:
+    print(dp[n])
 
 # 카드 구매하기
 
